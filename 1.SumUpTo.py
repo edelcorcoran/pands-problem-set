665c577622ccea8200c70bff82ea09a5a0c9d781
edelcorcoran <G00010824@gmit.ie>
2019-02-27 23:04:11 +0000

    Created Solution 1 SumUpTo.py

--- 1.SumUpTo.py
+++ 1.SumUpTo.py
@@ -1,8 +1,11 @@
-#Solution to Problem 1
 #Edel Corcoran 
-#Calculate the sum of all numbers between 1 and n (a positive integer).
 
 n = int(input("Enter a positive integer: "))
 for i in range(1,n):
     sum_num = sum(range(1,n))
 print(sum_num)
+
+
+
+
+
