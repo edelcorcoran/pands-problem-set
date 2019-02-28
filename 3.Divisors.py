a85260da79343d78c2215ab45a12f5b46042b5e8
edelcorcoran <47307672+edelcorcoran@users.noreply.github.com>
2019-02-28 17:58:19 +0000

    Update 3.Divisors.py

--- 3.Divisors.py
+++ 3.Divisors.py
@@ -1,18 +1,17 @@
 #Edel Corcoran Solution 2 from Problem Sheet 
 #Programming & Scripting H.Dip Data Analytics
 
-# Adapted from Calculate the factorial of a number (My First Program Video). Replaced some code with divisible code (ie. with remainder of zero (eg. i%6 == 0))
+# Adapted from Code examples in lecture videos on Statements and Controlled Loops
 
-# Not Working Yet!!
+#Range at start works
+# Working but not printing number!!
 
-start = 10000
-ans = 1000
-i = 1
-
-while i <= start:
-    if i%6 == 0:
-        print("i")
-    elif i%12 == 0:
+for n in range(1000, 10000):
+    if n%6 == 0:
+        print("n, is divisible by 6 but not 12")
+    elif n%12 == 0:
+        print()
+    else:
         print()
         
     
