b673bf25fa5d31da3df175ab61bf7baa682b1b92
edelcorcoran <G00010824@gmit.ie>
2019-02-28 17:14:24 +0000

    Created 3.Divisors.py

---
+++ 3.Divisors.py
@@ -0,0 +1,18 @@
+#Edel Corcoran Solution 2 from Problem Sheet 
+#Programming & Scripting H.Dip Data Analytics
+
+# Adapted from Calculate the factorial of a number (My First Program Video). Replaced some code with divisible code (ie. with remainder of zero (eg. i%6 == 0))
+
+# Not Working Yet!!
+
+start = 10000
+ans = 1000
+i = 1
+
+while i <= start:
+    if i%6 == 0:
+        print("i")
+    elif i%12 == 0:
+        print()
+        
+    
