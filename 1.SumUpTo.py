fedc7a0e654be9c52ea19608bef146aac4816aa0
edelcorcoran <G00010824@gmit.ie>
2019-03-29 22:02:18 +0000

    1.SumUpTo.py

--- 1.SumUpTo.py
+++ 1.SumUpTo.py
@@ -1,5 +1,8 @@
-#Edel Corcoran 
+#Edel Corcoran Python Sum Up To Program 
 
+#Program will give the sum of all numbers between 1 and a positive integer provided by user
+
+# User inputs any positive integer
 n = int(input("Enter a positive integer: "))
 for i in range(1,n):
     sum_num = sum(range(1,n))
--- 2.Begins-With-T.py
+++ 2.Begins-With-T.py
@@ -1,12 +1,17 @@
-#Edel Corcoran 
-#Is today a day that begins with the letter T?
-#Adapted from Tuesday.py 
+#Edel Corcoran Begins With T program
 
-import datetime
+#Program will determine whether today a day that begins with the letter T?
+#References: Adapted from Tuesday.py 
 
-if datetime.datetime.today().weekday() == 1 or 3:
-    print("Yes! Today is a day that starts with the letter T")
-else:
+
+#Program imports the current weekday and determines answer "Yes!..." or "No!...."
+import datetime as dt
+    
+if dt.datetime.today().weekday() == 1 or 3:
     print("No! Today is not a day that starts with the letter T")
+else:
+    print("Yes! Today is a day that starts with the letter T")
+
+
 
     
