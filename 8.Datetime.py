8822512e53fe38d1f929473baa19f4f65fd6bf2f
edelcorcoran <G00010824@gmit.ie>
2019-03-29 20:33:14 +0000

    8.Datetime.py

--- 8.Datetime.py
+++ 8.Datetime.py
@@ -1,4 +1,12 @@
 
-today = datetime.date.today()
-text = "{today.day}/{today.month}/{today.year}".format(today=today)
-print(text)
+# Edel Corcoran Question 8 Problem Set 2019
+
+# Write a program that outputs today's date.
+
+# References: Week 6 lecture videos, https://docs.python.org/3/library/datetime.html?highlight=datetime
+
+import datetime as dt
+dt.datetime.now()
+
+dt.datetime.strftime(dt.datetime.now(), "%A  %B %d %Y at %H:%M %p")
+
