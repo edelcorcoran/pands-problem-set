b34d6f1c2469dad2094d06ef5fbd3fd5f515f775
edelcorcoran <G00010824@gmit.ie>
2019-03-30 23:39:27 +0000

    5.Primes.py

--- 5.Primes.py
+++ 5.Primes.py
@@ -1,18 +1,24 @@
 #Edel Corcoran 
 
-#Adapted from (https://stackoverflow.com/questions/18833759/python-prime-number-checker)
+#Adapted from (https://stackoverflow.com/questions/18833759/python-prime-number-checker), https://www.javatpoint.com/python-check-prime-number
 
-#Not Working Correct Yet!
+
 
 a=2
-num=int(input("Please enter an integer: "))
+num=int(input("Please enter a positive integer: "))     #User requestef to input a positive integer
 
-while num>a:
-    if num%a==0 & a!=num:
-        print('Is not Prime Number')
-        break
-    else:
-        print('Is a Prime Number')
-        a=(num)+1
-
-
+def prime(num):         #define function to evaluate if users number is prime or not
+    while num == a:
+        print('2 Is the only even Prime Number')
+        break           #stop result being repeatedly printed
+    while num>a:
+        for i in range(2,num):      #for any number great than 2
+            if (num % i) ==0:
+                print(num,'Is not a Prime Number')
+                break       #stop result being repeatedly printed
+        else:
+            print(num,'Is a Prime Number')
+            break       #stop result being repeatedly printed
+        
+prime(num)
+ 
