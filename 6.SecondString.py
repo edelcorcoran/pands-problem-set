1315776ac8991a91a952aa546680fef38e701bc5
edelcorcoran <G00010824@gmit.ie>
2019-03-31 03:16:41 +0100

    6.SecondString.py

--- 6.SecondString.py
+++ 6.SecondString.py
@@ -2,3 +2,15 @@
 
 # Program takes a user input string and outputs very second work
 
+# References: https://stackoverflow.com/questions/47085552/replace-every-second-word-with-the-word-hello-using-a-function
+
+string = input("Create your own sentence")      # instruct user to input a sentence of their choosing
+
+# format to skip every second word in the user's sentence
+#Use the split method to create a list. Then loop over the list with using enumerate which creates a counter.
+
+skip = " ".join(["{} ".format(word)         
+        for idx, word in enumerate(string.split())  
+        if idx % 2 == 0])
+# prints every second word in users sentence
+print(skip)
