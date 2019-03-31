ca870560e906e78289e1194adbf8891315a222c2
edelcorcoran <G00010824@gmit.ie>
2019-03-31 00:02:26 +0000

    6.SecondString.py

---
+++ 6.SecondString.py
@@ -0,0 +1,4 @@
+# Edel Corcoran Solution 6 SecondString.py
+
+# Program takes a user input string and outputs very second work
+
---
+++ 7.SquareRoot.py
@@ -0,0 +1,4 @@
+
+# Edel Corcoran Solution 7 SquareRoot.py
+
+# Program takes a positive floating point number as input and outputs an approximation of its square root.
