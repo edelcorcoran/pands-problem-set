10d5f9d0fcadacf3d75050e1a3a15c4b7cdf94e7
edelcorcoran <G00010824@gmit.ie>
2019-03-31 23:40:41 +0100

    2.Begins-With-T.py

--- 1.SumUpTo.py
+++ 1.SumUpTo.py
@@ -1,4 +1,4 @@
-#Edel Corcoran Python Sum Up To Program 
+#Edel Corcoran Solution 1: Python Sum Up To Program 
 
 #Program will give the sum of all numbers between 1 and a positive integer provided by user
 
@@ -6,9 +6,9 @@
 
 # User inputs any positive integer
 n = int(input("Enter a positive integer: "))
-for i in range(1,n):
-    sum_num = sum(range(1,n))
-print(sum_num)
+for i in range(1,n):        #range is between 1 and number input by user
+    sum_num = sum(range(1,n))       #sum function to sum numbers in range
+print(sum_num)                  #print answer
 
 
 
--- 2.Begins-With-T.py
+++ 2.Begins-With-T.py
@@ -5,12 +5,14 @@
 
 
 #Program imports the current weekday and determines answer "Yes!..." or "No!...."
+
+#import datetime
 import datetime as dt
     
-if dt.datetime.today().weekday() == 1 or 3:
-    print("No! Today is not a day that starts with the letter T")
+if dt.datetime.today().weekday() == 1 or 3:         #0-6 are weekdays so Tuesday(1) and Thurday (3)
+    print("No! Today is not a day that starts with the letter T")  #if today is Tuesday or Thursday print ""No! Today is not a day that starts with the letter T""
 else:
-    print("Yes! Today is a day that starts with the letter T")
+    print("Yes! Today is a day that starts with the letter T")      #otherwise print "Yes! Today is a day that starts with the letter T"
 
 
 
